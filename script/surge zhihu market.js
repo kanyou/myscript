1fc190b4e1e8f4a2284fb24b9fead188b23cec47
kanyou <yanjingcy95@gmail.com>
2019-11-16 17:29:10 +0800

    zhihu

---
+++ script/surge zhihu market.js
@@ -0,0 +1,9 @@
+/**
+ *@supported 9E1BBA07A0EF
+ */
+let body = $response.body 
+body=JSON.parse(body)
+body['sub_webs'].splice(0,1)
+body['sub_webs'].splice(1,1)
+body=JSON.stringify(body)
+$done({body})
