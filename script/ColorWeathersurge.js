5cf36f51602f37421b59f4ba4455090d871c5844
kanyou <yanjingcy95@gmail.com>
2019-11-18 09:49:44 +0800

    ColorWeathersurge.js

--- script/ColorWeathersurge.js
+++ script/ColorWeathersurge.js
@@ -2,8 +2,8 @@
  *@supported 9E1BBA07A0EF
  */
 let obj = JSON.parse($response.body);
-obj.data["xy_vip_expire"] = 1700238229.113103;
-obj.data["is_vip"] = true;
-obj.data["vip_expired_at"] = 1700238229.113103;
-obj.data["is_xy_vip"] = true;
+obj.result["xy_vip_expire"] = 1700238229.113103;
+obj.result["is_vip"] = true;
+obj.result["vip_expired_at"] = 1700238229.113103;
+obj.result["is_xy_vip"] = true;
 $done({body: JSON.stringify(obj)});
