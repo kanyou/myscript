03cf62b242ee7c57f69f438c6e5de7dd388b1a8f
kanyou <43438535+kanyou@users.noreply.github.com>
2019-11-18 23:09:56 +0800

    Update pocketlist.js

--- script/pocketlist.js
+++ script/pocketlist.js
@@ -1,13 +1,14 @@
 var body = $response.body;
 var url = $request.url;
-var obj = JSON.parse(body);
-obj = {
-	"data":{
-	  "subscription_type":"premium",
-	  "subscription_expires":"2022-12-30"
-	},
-};
-$done({body: JSON.stringify(obj)});
+const path1 = '/api/v1/pocketlists.me.get';
+let obj = JSON.parse(body);
+if (url.indexOf(path1) != -1) {
+	obj.data["subscription_type"] = "premium";
+	obj.data["subscription_expires"] = "2022-12-30";
+	body = JSON.stringify(obj);  
+ }
+
+$done({body});
 /**
  * @supported 9E1BBA07A0EF
  */
