09d664a61ecbde2879369cb5cba809a9e470ade7
kanyou <43438535+kanyou@users.noreply.github.com>
2019-11-18 22:31:54 +0800

    Update pocketlist.js

--- script/pocketlist.js
+++ script/pocketlist.js
@@ -2,7 +2,12 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 obj = {
+	"data":{
 	"subscription_type": "premium",
 	"subscription_expires" = "2022-12-30";
+	};
 };
 $done({body: JSON.stringify(obj)});
+/**
+ * @supported 9E1BBA07A0EF
+ */
