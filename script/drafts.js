ce32a5d6d13eb914b97fa4658460af54acdecc36
kanyou <43438535+kanyou@users.noreply.github.com>
2019-11-22 21:20:45 +0800

    dr

---
+++ script/drafts.js
@@ -0,0 +1,5 @@
+const bodyObj = json.parse($response.body)}
+bodyObj.data.is_subscription_active=true;
+bodyObj.data.active_expires_at="2022-01001T00:00:00Z";
+bodyObj.data.active_subscription_type=premium
+$done({body:JSON.stringify(bodyObj)})
