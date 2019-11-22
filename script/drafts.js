954ad817214ded7c08e3a48b84f51a626bbb19be
kanyou <43438535+kanyou@users.noreply.github.com>
2019-11-22 21:37:08 +0800

    Update drafts.js

--- script/drafts.js
+++ script/drafts.js
@@ -1,4 +1,4 @@
-const bodyObj = json.parse($response.body)};
+const bodyObj = json.parse($response.body);
 bodyObj.data.is_subscription_active=true;
 bodyObj.data.active_expires_at="2022-01001T00:00:00Z";
 bodyObj.data.active_subscription_type="premium";
