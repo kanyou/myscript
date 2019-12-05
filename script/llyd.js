584e1244076de01980202d39c657e61460a0dbce
kanyou <yanjingcy95@gmail.com>
2019-12-05 23:32:52 +0800

    llyd

--- script/llyd.js
+++ script/llyd.js
@@ -1,3 +1,4 @@
+if ($response.statusCode == 200 && $request.method == 'GET') {
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
@@ -10,13 +11,14 @@ if (url.indexOf(vip) != -1) {
 	obj["auditionDuration"] = 7200;
 	body = JSON.stringify(obj);
 }
-
 if (url.indexOf(time) != -1) {
 	obj["from"] = 1482071586;
 	obj["to"] = 1671373986;
 	body = JSON.stringify(obj);
 }
-
+} else {
+	$done({})
+}
 $done({body});
 /**
  * @supported 9E1BBA07A0EF
